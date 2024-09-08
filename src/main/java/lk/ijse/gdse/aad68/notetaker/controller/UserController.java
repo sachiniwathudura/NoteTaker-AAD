d4d373553a92f37d05ccc001d2a89f3ad8ec280f
sachiniwathudura <sachiniwathudura@gmail.com>
2024-09-08 14:10:16 +0530

    updateUser process

--- src/main/java/lk/ijse/gdse/aad68/notetaker/controller/UserController.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/controller/UserController.java
@@ -52,5 +52,28 @@ public class UserController {
     public List<UserDTO> getAllUsers(){
         return userService.getAllUsers();
     }
+    @PatchMapping(value = "/{id}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> updateUser(
+//        return userService.updateUser(id,userDTO)? new ResponseEntity<>(HttpStatus.NO_CONTENT):new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        @PathVariable("id") String id,
+        @RequestPart("updatefirstName") String updatefirstName,
+        @RequestPart ("updatelastName") String updatelastName,
+        @RequestPart ("updateemail") String updateemail,
+        @RequestPart ("updatepassword") String updatepassword,
+        @RequestPart ("updateprofilePic") String updateprofilePic) {
+
+            // Handle profile pic
+            String base64ProfilePic = AppUtil.toBase64ProfilePic(updateprofilePic);
+            // build the user object,multi part awa data tika dto ekt set krgnnwa
+           var updateUser = new UserDTO();
+           updateUser.setUserId(id);
+            updateUser.setFirstName(updatefirstName);
+            updateUser.setLastName(updatelastName);
+            updateUser.setEmail(updateemail);
+            updateUser.setPassword(updatepassword);
+            updateUser.setProfilePic(base64ProfilePic);
+            //send to the service layer
+        return userService.updateUser(updateUser)? new ResponseEntity<>(HttpStatus.NO_CONTENT): new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
 }
